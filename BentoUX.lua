46003610e6327ed2d7fc31e391c20ddb690ed619
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2024-09-26 15:02:19 +0200

    Refactor code to hide XP and status bars based on player level

--- BentoUX.lua
+++ BentoUX.lua
@@ -198,12 +198,22 @@ ChatButtonEvents:SetScript("OnEvent", HideChatButtons)
 
 
 
--- HIDE STATUS TRACKING BARS
+-- HIDE XP AND STATUS BARS
 
 local function HideStatusTrackingBars()
-    if MainStatusTrackingBarContainer then
-        MainStatusTrackingBarContainer:Hide()
-        MainStatusTrackingBarContainer:SetScript("OnShow", MainStatusTrackingBarContainer.Hide)
+    local playerLevel = UnitLevel("player")
+    local maxLevel = GetMaxPlayerLevel()
+
+    if playerLevel < maxLevel then
+        if MainStatusTrackingBarContainer then
+            MainStatusTrackingBarContainer:Show()
+            MainStatusTrackingBarContainer:SetScript("OnShow", nil)
+        end
+    else
+        if MainStatusTrackingBarContainer then
+            MainStatusTrackingBarContainer:Hide()
+            MainStatusTrackingBarContainer:SetScript("OnShow", MainStatusTrackingBarContainer.Hide)
+        end
     end
 
     if SecondaryStatusTrackingBarContainer then
@@ -214,6 +224,7 @@ end
 
 local StatusTrackingEvents = CreateFrame("Frame")
 StatusTrackingEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+StatusTrackingEvents:RegisterEvent("PLAYER_LEVEL_UP")
 StatusTrackingEvents:SetScript("OnEvent", function(self, event)
     C_Timer.After(1, HideStatusTrackingBars)
 end)
