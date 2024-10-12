8beb7f44e8c9e13f1021e6d2743758590f69e0ca
Marco Vrinssen <marcovrinssen@icloud.com>
2024-10-12 13:55:12 +0200

    Update

--- Config.lua
+++ Config.lua
@@ -14,7 +14,7 @@ local function SetupCVar()
     SetCVar("nameplateMotionSpeed", 0.05)
     SetCVar("nameplateOverlapV", 0.5)
 
-    SetCVar("mouseAcceleration", 0)
+    SetCVar("mouseAcceleration", -1)
 
     SetCVar("autoLootRate", 0.001)
 end
