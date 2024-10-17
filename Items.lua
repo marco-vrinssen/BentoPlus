85e4928e5d95ef69a59ff4566135bbebeba9922f
Marco Vrinssen <marcovrinssen@icloud.com>
2024-10-17 17:21:15 +0200

    Refactor and add faster looting function

--- Items.lua
+++ Items.lua
@@ -9,3 +9,19 @@ end
 local MerchantEvents = CreateFrame("Frame")
 MerchantEvents:SetScript("OnEvent", AutoSellRepair)
 MerchantEvents:RegisterEvent("MERCHANT_SHOW")
+
+
+
+
+-- AUTO LOOT ALL ITEMS
+local function AutoLootAllItems()
+    if GetCVarBool("autoLootDefault") and not IsModifiedClick("AUTOLOOTTOGGLE") then
+        for i = GetNumLootItems(), 1, -1 do
+            LootSlot(i)
+        end
+    end
+end
+
+local LootFrame = CreateFrame("Frame")
+LootFrame:SetScript("OnEvent", AutoLootAllItems)
+LootFrame:RegisterEvent("LOOT_READY")
