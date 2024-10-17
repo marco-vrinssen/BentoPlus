a3a261b787c3e91510fe24e34d13c5507bfc98b3
Marco Vrinssen <marcovrinssen@icloud.com>
2024-10-17 21:27:01 +0200

    Remove faster loot function

--- Items.lua
+++ Items.lua
@@ -11,21 +11,3 @@ end
 local MerchantEvents = CreateFrame("Frame")
 MerchantEvents:SetScript("OnEvent", AutoSellRepair)
 MerchantEvents:RegisterEvent("MERCHANT_SHOW")
-
-
-
-
--- SPEED UP AUTO LOOTING
-
-local function AutoLootAllItems()
-    if GetCVar("autoLootDefault") == "1" and not IsModifiedClick("AUTOLOOTTOGGLE") then
-        for i = GetNumLootItems(), 1, -1 do
-            LootSlot(i)
-        end
-        LootFrame:Hide()
-    end
-end
-
-local LootFrame = CreateFrame("Frame")
-LootFrame:SetScript("OnEvent", AutoLootAllItems)
-LootFrame:RegisterEvent("LOOT_READY")
