d3db12d1dc77ca249e635a8495d4b789cdf2b0a6
Marco Vrinssen <marcovrinssen@icloud.com>
2024-10-18 15:21:19 +0200

    Refactor

--- Items.lua
+++ Items.lua
@@ -11,3 +11,20 @@ end
 local MerchantEvents = CreateFrame("Frame")
 MerchantEvents:SetScript("OnEvent", AutoSellRepair)
 MerchantEvents:RegisterEvent("MERCHANT_SHOW")
+
+
+
+
+-- SPEED UP AUTO LOOTING ITEMS
+
+local function AutoLootItems()
+    if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+        for i = GetNumLootItems(), 1, -1 do
+            LootSlot(i)
+        end
+    end
+end
+
+local LootEvents = CreateFrame("Frame")
+LootEvents:SetScript("OnEvent", AutoLootItems)
+LootEvents:RegisterEvent("LOOT_READY")
