87fa6efecbe3b1eb7de61895f93e76be247dcf42
Marco Vrinssen <marcovrinssen@icloud.com>
2024-10-20 16:11:45 +0200

    Update

--- Chat.lua
+++ Chat.lua
@@ -1,21 +1,31 @@
--- -- HIDE CHAT BUTTONS WHEN PLAYER ENTERS WORLD
+-- HIDE CHAT BUTTONS WHEN PLAYER ENTERS WORLD
 
--- local function HideChatButtons()
---     ChatFrameChannelButton:Hide()
---     ChatFrameToggleVoiceDeafenButton:Hide()
---     ChatFrameToggleVoiceMuteButton:Hide()
---     ChatFrameMenuButton:Hide()
+local function HideChatButtons()
+    ChatFrameChannelButton:Hide()
+    ChatFrameToggleVoiceDeafenButton:Hide()
+    ChatFrameToggleVoiceMuteButton:Hide()
+    ChatFrameMenuButton:Hide()
 
---     for i = 1, 16 do
---         local chatFrameButtonFrame = _G["ChatFrame" .. i .. "ButtonFrame"]
---         if chatFrameButtonFrame then
---             chatFrameButtonFrame:Hide()
---             chatFrameButtonFrame:SetScript("OnShow", chatFrameButtonFrame.Hide)
---         end
---     end
--- end
+    for i = 1, 16 do
+        local chatFrameButtonFrame = _G["ChatFrame" .. i .. "ButtonFrame"]
+        if chatFrameButtonFrame then
+            chatFrameButtonFrame:Hide()
+            chatFrameButtonFrame:SetScript("OnShow", chatFrameButtonFrame.Hide)
+        end
+    end
+end
 
--- local ChatButtonEvents = CreateFrame("Frame")
--- ChatButtonEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
--- ChatButtonEvents:RegisterEvent("CHAT_MSG_WHISPER")
--- ChatButtonEvents:SetScript("OnEvent", HideChatButtons)
+local function RepositionQuickJoinToastButton()
+    QuickJoinToastButton:ClearAllPoints()
+    QuickJoinToastButton:SetPoint("TOPRIGHT", GeneralDockManager, "TOPLEFT", 0, -4)
+end
+
+local function OnEvent(self, event, ...)
+    HideChatButtons()
+    RepositionQuickJoinToastButton()
+end
+
+local ChatButtonEvents = CreateFrame("Frame")
+ChatButtonEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+ChatButtonEvents:RegisterEvent("CHAT_MSG_WHISPER")
+ChatButtonEvents:SetScript("OnEvent", OnEvent)
