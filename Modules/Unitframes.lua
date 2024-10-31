796720135e530990af9fe8176dc816a30b8ad726
Marco Vrinssen <marcovrinssen@icloud.com>
2024-10-31 17:56:59 +0100

    Consolidate functions to hide target and focus frame auras along with PvP badges

--- Modules/Unitframes.lua
+++ Modules/Unitframes.lua
@@ -1,36 +1,19 @@
--- Hide target and focus frame auras
+-- Hide target and focus frame auras and PvP badges
 
-local function HideTargetFrameAuras()
+local function HideAllElements()
+    -- Hide target and focus frame auras
     MAX_TARGET_BUFFS = 0
     MAX_TARGET_DEBUFFS = 0
-end
-
-local function HideFocusFrameAuras()
     MAX_FOCUS_BUFFS = 0
     MAX_FOCUS_DEBUFFS = 0
-end
 
-local function HideAllAuras()
-    HideTargetFrameAuras()
-    HideFocusFrameAuras()
-end
-
-local UnitFrameEvents = CreateFrame("Frame")
-UnitFrameEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-UnitFrameEvents:SetScript("OnEvent", HideAllAuras)
-
-
-
-
--- Hide PvP badges on target and player frames
-
-local function HidePvPBadges()
+    -- Hide PvP badges on target and player frames
     PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PrestigeBadge:Hide()
     PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PrestigePortrait:Hide()
     TargetFrame.TargetFrameContent.TargetFrameContentContextual.PrestigeBadge:Hide()
     TargetFrame.TargetFrameContent.TargetFrameContentContextual.PrestigePortrait:Hide()
 end
 
-local PvPBadgeEvents = CreateFrame("Frame")
-PvPBadgeEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-PvPBadgeEvents:SetScript("OnEvent", HidePvPBadges)
+local UnitFrameEvents = CreateFrame("Frame")
+UnitFrameEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+UnitFrameEvents:SetScript("OnEvent", HideAllElements)
