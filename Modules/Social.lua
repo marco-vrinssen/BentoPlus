1fd91161015d3983ff1ff1333e1a7ccde3acb2a6
Marco Vrinssen <marcovrinssen@icloud.com>
2024-10-31 11:58:47 +0100

    Remove Social module and associated QuickJoinToastButton functionality

--- Modules/Social.lua
+++
@@ -1,6 +0,0 @@
--- Move the QuickJoinToastButton to the top left corner with a margin of 24
-QuickJoinToastButton:ClearAllPoints()
-QuickJoinToastButton:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 24, -24)
-
--- Reduce the alpha of the QuickJoinToastButton to 50%
-QuickJoinToastButton:SetAlpha(0)
