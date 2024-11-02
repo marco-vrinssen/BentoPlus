ee296156e3e8d1dad82db2abc445a16adae7f2cd
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-02 17:03:07 +0100

    Update camera distance max zoom factor in Graphics module for enhanced visual experience

--- Modules/Graphics.lua
+++ Modules/Graphics.lua
@@ -4,6 +4,7 @@ local function UpdateGraphicsConfig()
     SetCVar("ffxGlow", 0)
     SetCVar("ffxDeath", 0)
     SetCVar("ffxNether", 0)
+    
     SetCVar("cameraDistanceMaxZoomFactor", 2.6)
 end
 
