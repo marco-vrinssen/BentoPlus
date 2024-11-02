1767229853513aba8079a1c2f98b870cec5a836b
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-02 10:15:06 +0100

    Refactor Unitframes module to separate aura and prestige badge hiding functions for improved readability and event handling

--- Modules/Unitframes.lua
+++ Modules/Unitframes.lua
@@ -1,19 +1,28 @@
 -- Hide target and focus frame auras and PvP badges
 
-local function HideAllElements()
-    -- Hide target and focus frame auras
+local function HideAuras()
     MAX_TARGET_BUFFS = 0
     MAX_TARGET_DEBUFFS = 0
     MAX_FOCUS_BUFFS = 0
     MAX_FOCUS_DEBUFFS = 0
+end
 
-    -- Hide PvP badges on target and player frames
+local function HidePrestigeBadges()
     PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PrestigeBadge:Hide()
     PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PrestigePortrait:Hide()
     TargetFrame.TargetFrameContent.TargetFrameContentContextual.PrestigeBadge:Hide()
     TargetFrame.TargetFrameContent.TargetFrameContentContextual.PrestigePortrait:Hide()
+    TargetFrame.TargetFrameContent.TargetFrameContentContextual.PvpIcon:Hide()
 end
 
 local UnitFrameEvents = CreateFrame("Frame")
 UnitFrameEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-UnitFrameEvents:SetScript("OnEvent", HideAllElements)
+UnitFrameEvents:RegisterEvent("PLAYER_TARGET_CHANGED")
+UnitFrameEvents:SetScript("OnEvent", function(self, event, ...)
+    if event == "PLAYER_ENTERING_WORLD" then
+        HideAuras()
+        HidePrestigeBadges()
+    elseif event == "PLAYER_TARGET_CHANGED" then
+        HidePrestigeBadges()
+    end
+end)
