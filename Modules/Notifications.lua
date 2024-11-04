c8ce8d31f320e67580b6857da97ee30019bcf19e
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-04 15:27:38 +0100

    Refactor notification positioning to improve toast element anchoring and streamline updates

--- Modules/Notifications.lua
+++ Modules/Notifications.lua
@@ -7,45 +7,37 @@ end)
 
 
 -- Create a new frame in the top left corner to attach the BNToastFrame and QuickJoinToastButton
-
 local NotificationFrame = CreateFrame("Frame", "NotificationFrame", UIParent)
 NotificationFrame:SetSize(200, 100)
 NotificationFrame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 24, -24)
 
 local function UpdateToastElements()
-
     QuickJoinToastButton:Hide()
 
     if QuickJoinToastButton.Toast1 then
         QuickJoinToastButton.Toast1:ClearAllPoints()
         QuickJoinToastButton.Toast1:SetParent(NotificationFrame)
-        QuickJoinToastButton.Toast1:SetPoint("TOPLEFT", NotificationFrame, "TOPLEFT", 0, 0)
+        QuickJoinToastButton.Toast1:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 0, 0)
     end
 
     if QuickJoinToastButton.Toast2 then
         QuickJoinToastButton.Toast2:ClearAllPoints()
         QuickJoinToastButton.Toast2:SetParent(NotificationFrame)
-        QuickJoinToastButton.Toast2:SetPoint("TOPLEFT", QuickJoinToastButton.Toast1 or NotificationFrame, "BOTTOMLEFT", 0, -8)
+        QuickJoinToastButton.Toast2:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", 0, 0)
     end
+
+    BNToastFrame:ClearAllPoints()
+    BNToastFrame:SetParent(NotificationFrame)
+    BNToastFrame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 0, 0)
+end
+
+local function OnPlayerEnteringWorld()
+    UpdateToastElements()
 end
 
 local NotificationEvents = CreateFrame("Frame")
 NotificationEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 NotificationEvents:SetScript("OnEvent", OnPlayerEnteringWorld)
 
+BNToastFrame:HookScript("OnShow", UpdateToastElements)
 QuickJoinToastButton:HookScript("OnShow", UpdateToastElements)
-QuickJoinToastButton.Toast1:HookScript("OnShow", UpdateToastElements)
-QuickJoinToastButton.Toast2:HookScript("OnShow", UpdateToastElements)
-
-
-
-
--- Function to re-anchor the BNToastFrame
-
-local function RepositionBNToastFrame()
-    BNToastFrame:ClearAllPoints()
-    BNToastFrame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 24, -24)
-end
-
--- Hook the function to the BNToastFrame's OnShow event
-BNToastFrame:HookScript("OnShow", RepositionBNToastFrame)
