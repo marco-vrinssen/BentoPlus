1a78738ae99b27823334c341daa6efb10ccac8d0
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-05 09:25:45 +0100

    Refactor auto looting logic to streamline frame hiding and improve efficiency

--- Modules/Loot.lua
+++ Modules/Loot.lua
@@ -13,21 +13,19 @@ LootConfigFrame:SetScript("OnEvent", UpdateLootRate)
 
 -- Speed up auto looting while hiding the loot frame during the looting
 
-local LootEpoch = 0
-local LootDelay = 0.1
-
-local function FasterLooting()
-	if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
-		if (GetTime() - LootEpoch) >= LootDelay then
-			for i = GetNumLootItems(), 1, -1 do
-				LootSlot(i)
-			end    
-			LootEpoch = GetTime()
-            LootFrame:Hide()
-		end
-	end
+local function UpdateAutoLoot()
+    if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
+        for i = GetNumLootItems(), 1, -1 do
+            LootSlot(i)
+        end
+        for name, frame in pairs(_G) do
+            if type(frame) == "table" and frame.Hide and name:find("LootFrame") then
+                frame:Hide()
+            end
+        end
+    end
 end
 
 local LootTrigger = CreateFrame("Frame")
 LootTrigger:RegisterEvent("LOOT_READY")
-LootTrigger:SetScript("OnEvent", FasterLooting)
+LootTrigger:SetScript("OnEvent", UpdateAutoLoot)
