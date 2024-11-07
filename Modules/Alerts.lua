da4315feab0ef0dfab10a7c1476ee71c80dbaaa6
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-07 10:01:47 +0100

    Add alert management for Talking Head frame and reposition notification toasts

--- Modules/Alerts.lua
+++ Modules/Alerts.lua
@@ -8,3 +8,51 @@ end
 local AlertEvents = CreateFrame("Frame")
 AlertEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 AlertEvents:SetScript("OnEvent", MuteAndHideAllAlerts)
+
+
+
+
+-- Hide Talking Head frame
+hooksecurefunc(TalkingHeadFrame, "PlayCurrent", function(self)
+    self:Hide()
+end)
+
+
+
+
+-- Reposition BattleNet and Quickjoin Toasts
+
+local NotificationFrame = CreateFrame("Frame", "NotificationFrame", UIParent)
+NotificationFrame:SetSize(200, 100)
+NotificationFrame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 24, -24)
+
+local function UpdateToastElements()
+    QuickJoinToastButton:SetAlpha(0)
+
+    if QuickJoinToastButton.Toast1 then
+        QuickJoinToastButton.Toast1:ClearAllPoints()
+        QuickJoinToastButton.Toast1:SetParent(NotificationFrame)
+        QuickJoinToastButton.Toast1:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 0, 0)
+    end
+
+    if QuickJoinToastButton.Toast2 then
+        QuickJoinToastButton.Toast2:ClearAllPoints()
+        QuickJoinToastButton.Toast2:SetParent(NotificationFrame)
+        QuickJoinToastButton.Toast2:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", 0, 0)
+    end
+
+    BNToastFrame:ClearAllPoints()
+    BNToastFrame:SetParent(NotificationFrame)
+    BNToastFrame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 0, 0)
+end
+
+local function OnPlayerEnteringWorld()
+    UpdateToastElements()
+end
+
+local NotificationEvents = CreateFrame("Frame")
+NotificationEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+NotificationEvents:SetScript("OnEvent", OnPlayerEnteringWorld)
+
+BNToastFrame:HookScript("OnShow", UpdateToastElements)
+QuickJoinToastButton:HookScript("OnShow", UpdateToastElements)
