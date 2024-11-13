54adf59bd64323711c076c3655e0b21f03b7d676
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-13 17:31:00 +0100

    Refactor aura and badge hiding logic in Unitframes module

--- Modules/Unitframes.lua
+++ Modules/Unitframes.lua
@@ -1,11 +1,6 @@
 -- Hide target and focus frame auras and PvP badges
 
-local function HideAurasAndBadges()
-    MAX_TARGET_BUFFS = 0
-    MAX_TARGET_DEBUFFS = 0
-    MAX_FOCUS_BUFFS = 0
-    MAX_FOCUS_DEBUFFS = 0
-
+local function HideBadges()
     PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PrestigeBadge:Hide()
     PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PrestigePortrait:Hide()
 
@@ -16,9 +11,4 @@ end
 local UnitFrameEvents = CreateFrame("Frame")
 UnitFrameEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 UnitFrameEvents:RegisterEvent("PLAYER_TARGET_CHANGED")
-UnitFrameEvents:RegisterEvent("UNIT_AURA")
-UnitFrameEvents:SetScript("OnEvent", function(self, event, ...)
-    if event == "PLAYER_ENTERING_WORLD" or event == "PLAYER_TARGET_CHANGED" or event == "UNIT_AURA" then
-        HideAurasAndBadges()
-    end
-end)
+UnitFrameEvents:SetScript("OnEvent", HideBadges)
