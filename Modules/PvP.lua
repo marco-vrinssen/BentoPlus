38392fd152824f935e0a17c8f9a4c185dba8f51f
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-19 14:44:31 +0100

    Refactor PvP module for improved readability and consistency in function naming

--- Modules/PvP.lua
+++ Modules/PvP.lua
@@ -1,6 +1,6 @@
--- Rebind Tab key based on the player's environment (PvP or PvE)
+-- Rebind Tab key based on the player's environment (PvP or PvE
 
-local function RebindTabKey()
+local function rebindTabKey()
     local inInstance, instanceType = IsInInstance()
     local pvpType = C_PvP.GetZonePVPInfo()
     
@@ -31,52 +31,52 @@ local function RebindTabKey()
     end
 end
 
-local TabBindEvents = CreateFrame("Frame")
-TabBindEvents:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-TabBindEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
-TabBindEvents:SetScript("OnEvent", RebindTabKey)
+local tabBindEvents = CreateFrame("Frame")
+tabBindEvents:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+tabBindEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+tabBindEvents:SetScript("OnEvent", rebindTabKey)
 
 
 -- Display the time remaining in the PvP queue dialog
 
-local TimeLeft = -1
+local timeLeft = -1
 
-local QueueTimer = PVPReadyDialog:CreateFontString(nil, "ARTWORK")
-QueueTimer:SetFontObject(GameFontNormal)
-QueueTimer:SetFont(GameFontNormal:GetFont(), 32, "OUTLINE")
-QueueTimer:SetTextColor(1, 1, 1)
-QueueTimer:SetPoint("TOP", PVPReadyDialog, "BOTTOM", 0, -16)
+local queueTimer = PVPReadyDialog:CreateFontString(nil, "ARTWORK")
+queueTimer:SetFontObject(GameFontNormal)
+queueTimer:SetFont(GameFontNormal:GetFont(), 32, "OUTLINE")
+queueTimer:SetTextColor(1, 1, 1)
+queueTimer:SetPoint("TOP", PVPReadyDialog, "BOTTOM", 0, -16)
 
-local function UpdatePvPTimer(self, elapsed)
-    TimeLeft = TimeLeft - elapsed
-    if TimeLeft > 0 then
-        QueueTimer:SetText(tostring(floor(TimeLeft + 0.5)))
+local function updatePvPTimer(self, elapsed)
+    timeLeft = timeLeft - elapsed
+    if timeLeft > 0 then
+        queueTimer:SetText(tostring(floor(timeLeft + 0.5)))
     else
-        QueueTimer:Hide()
+        queueTimer:Hide()
         self:SetScript("OnUpdate", nil)
     end
 end
 
 hooksecurefunc("PVPReadyDialog_Display", function(self, id)
-    TimeLeft = GetBattlefieldPortExpiration(id)
-    if TimeLeft and TimeLeft > 0 then
-        PVPReadyDialog:SetScript("OnUpdate", UpdatePvPTimer)
-        QueueTimer:Show()
+    timeLeft = GetBattlefieldPortExpiration(id)
+    if timeLeft and timeLeft > 0 then
+        PVPReadyDialog:SetScript("OnUpdate", updatePvPTimer)
+        queueTimer:Show()
     else
-        QueueTimer:Hide()
+        queueTimer:Hide()
         PVPReadyDialog:SetScript("OnUpdate", nil)
     end
 end)
 
 PVPReadyDialog:HookScript("OnHide", function()
-    QueueTimer:Hide()
+    queueTimer:Hide()
     PVPReadyDialog:SetScript("OnUpdate", nil)
 end)
 
 
 -- Automatically release the ghost in PvP zones
 
-local function AutoReleaseGhost()
+local function autoReleaseGhost()
     if C_DeathInfo.GetSelfResurrectOptions() and #C_DeathInfo.GetSelfResurrectOptions() > 0 then
         return
     end
@@ -87,13 +87,13 @@ local function AutoReleaseGhost()
     if (instanceType == "pvp" or pvpType == "combat") then
         C_Timer.After(0.5, function()
             local deathDialog = StaticPopup_FindVisible("DEATH")
-            if deathDialog and deathDialog.button1:IsEnabled() then
-                deathDialog.button1:Click()
+            if deathDialog and deathDialog.Button1:IsEnabled() then
+                deathDialog.Button1:Click()
             end
         end)
     end
 end
 
-local ReleaseEvents = CreateFrame("Frame")
-ReleaseEvents:RegisterEvent("PLAYER_DEAD")
-ReleaseEvents:SetScript("OnEvent", AutoReleaseGhost)
+local releaseEvents = CreateFrame("Frame")
+releaseEvents:RegisterEvent("PLAYER_DEAD")
+releaseEvents:SetScript("OnEvent", autoReleaseGhost)
