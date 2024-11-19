b19ed23207fcee2688ef21c31fa8852f6734ba0a
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-19 14:44:26 +0100

    Refactor alert handling to mute and hide all alerts on player entering world

--- Modules/Miscellaneous.lua
+++ Modules/Miscellaneous.lua
@@ -1,17 +1,15 @@
 -- Function to mute and hide all alerts by unregistering events and muting sound
-
 local function MuteAndHideAllAlerts()
     MuteSoundFile(569143)
     AlertFrame:UnregisterAllEvents()
 end
 
+-- Create a frame to handle alert events
 local AlertEvents = CreateFrame("Frame")
 AlertEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
 AlertEvents:SetScript("OnEvent", MuteAndHideAllAlerts)
 
-
 -- Hide vehicle seat indicator
-
 local VehicleSeatIndicator = _G["VehicleSeatIndicator"]
 VehicleSeatIndicator:Hide()
 VehicleSeatIndicator:SetScript("OnShow", VehicleSeatIndicator.Hide)
