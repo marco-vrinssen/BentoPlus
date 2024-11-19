c25fb24a06d8fc3720a1e97bb8f3dcf0ab74aeea
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-19 09:00:51 +0100

    Add functionality to mute and hide alerts, and hide vehicle seat indicator

---
+++ Modules/Miscellaneous.lua
@@ -0,0 +1,17 @@
+-- Function to mute and hide all alerts by unregistering events and muting sound
+
+local function MuteAndHideAllAlerts()
+    MuteSoundFile(569143)
+    AlertFrame:UnregisterAllEvents()
+end
+
+local AlertEvents = CreateFrame("Frame")
+AlertEvents:RegisterEvent("PLAYER_ENTERING_WORLD")
+AlertEvents:SetScript("OnEvent", MuteAndHideAllAlerts)
+
+
+-- Hide vehicle seat indicator
+
+local VehicleSeatIndicator = _G["VehicleSeatIndicator"]
+VehicleSeatIndicator:Hide()
+VehicleSeatIndicator:SetScript("OnShow", VehicleSeatIndicator.Hide)
