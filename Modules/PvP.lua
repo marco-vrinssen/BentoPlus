baada33693ff08a04c7141cffeaeffc7e853daee
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-19 09:00:59 +0100

    Increase delay before auto-releasing ghost to improve user experience

--- Modules/PvP.lua
+++ Modules/PvP.lua
@@ -85,7 +85,7 @@ local function AutoReleaseGhost()
     local pvpType = C_PvP.GetZonePVPInfo()
 
     if (instanceType == "pvp" or pvpType == "combat") then
-        C_Timer.After(0, function()
+        C_Timer.After(0.5, function()
             local deathDialog = StaticPopup_FindVisible("DEATH")
             if deathDialog and deathDialog.button1:IsEnabled() then
                 deathDialog.button1:Click()
