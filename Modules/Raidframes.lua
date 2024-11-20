f566637405439433de57a086241f0aa4995bedfe
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-20 16:28:24 +0100

    Remove Raidframes module to streamline UI and eliminate unused functionality

--- Modules/Raidframes.lua
+++
@@ -1,8 +0,0 @@
--- Hide raid frame auras
-
-local function HideAllAuras(frame)
-    CompactUnitFrame_HideAllBuffs(frame)
-    CompactUnitFrame_HideAllDebuffs(frame)
-end
-
-hooksecurefunc("CompactUnitFrame_UpdateAuras", HideAllAuras)
