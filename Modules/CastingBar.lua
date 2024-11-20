56530926d5c907c72a550a7b734caff00b52a890
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-20 16:30:19 +0100

    Remove CastingBar module to simplify codebase and eliminate unused functionality

--- Modules/CastingBar.lua
+++
@@ -1,9 +0,0 @@
-local function UpdateCastingBar()
-    local castingBar = PlayerCastingBarFrame
-    castingBar.Text:Hide()
-    castingBar.TextBorder:Hide()
-end
-
-local eventFrame = CreateFrame("Frame")
-eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-eventFrame:SetScript("OnEvent", UpdateCastingBar)
