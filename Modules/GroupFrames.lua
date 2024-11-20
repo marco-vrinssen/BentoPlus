fa8e33e15a39ab6d4f0e4f8fcc606c7973ebcdfe
Marco Vrinssen <marcovrinssen@icloud.com>
2024-11-20 16:28:16 +0100

    Add GroupFrames module to hide raid frame auras

---
+++ Modules/GroupFrames.lua
@@ -0,0 +1,8 @@
+-- Hide raid frame auras
+
+local function HideAllAuras(frame)
+    CompactUnitFrame_HideAllBuffs(frame)
+    CompactUnitFrame_HideAllDebuffs(frame)
+end
+
+hooksecurefunc("CompactUnitFrame_UpdateAuras", HideAllAuras)
