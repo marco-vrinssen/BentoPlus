67c63abaa323f3c914721c1f096eaf549d4e6e7f
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-07-31 15:19:16 +0200

    Refactor command descriptions for clarity and consistency in the IntroMessage module

--- Modules/IntroMessage.lua
+++ Modules/IntroMessage.lua
@@ -2,13 +2,13 @@
 
 local function displayAvailableCommands()
   print("|cffffffffBentoPlus Available Commands:|r")
-  print("|cff00ff00/rl|r: Reload the UI with a graphics restart and cache clear")
-  print("|cff00ff00/ui|r: Reload the UI")
+  print("|cff00ff00/rl|r: Reload UI with graphics restart and cache clear")
+  print("|cff00ff00/ui|r: Reload UI")
   print("|cff00ff00/gx|r: Restart graphics")
-  print("|cff00ff00/errors|r: Toggle error display on/off")
-  print("|cff00ff00/raidframeauras|r: Toggle visibility of raid frame auras")
-  print("|cff00ff00/arenaframeelements|r: Toggle visibility of arena frame elements")
-  print("|cff00ff00/buttonglow|r: Toggle visibility of button glow effects")
+  print("|cff00ff00/errors|r: Toggle script error display")
+  print("|cff00ff00/raidframeauras|r: Toggle raid frame aura visibility")
+  print("|cff00ff00/arenaframeelements|r: Toggle arena frame element visibility")
+  print("|cff00ff00/buttonglow|r: Toggle button glow effect visibility")
   print("|cffffffffRight-click Main Menu button to reload UI|r")
 end
 
