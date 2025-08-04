328c544675ef8110eb9bfe7a8790a207cdebc474
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-08-04 14:07:07 +0200

    Remove deprecated modules: AlertBanners, ArenaFrames, AuctionHouse, AutoTransmog, AutoWarbandBank, Commands, HideButtonGlow, IntroMessage, Mounts, PlayerFrame, StatusBar, TargetFrame

--- Modules/Mounts.lua
+++
@@ -1,5 +0,0 @@
--- Hide the vehicle seat indicator and prevent it from reappearing.
-
-local vehicleSeatIndicator = _G["VehicleSeatIndicator"]
-vehicleSeatIndicator:Hide()
-vehicleSeatIndicator:SetScript("OnShow", vehicleSeatIndicator.Hide)
