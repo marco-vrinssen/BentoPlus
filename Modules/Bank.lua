254ef287ee25868890a9ef420a75bc13e6122e97
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-08-04 14:31:24 +0200

    Remove unused modules: Bank, ButtonGlow, CopyName, HideElements, RaidFrames, and Transmog

--- Modules/Bank.lua
+++
@@ -1,9 +0,0 @@
--- Automatically select the warband bank tab when the bank frame is opened.
-
-local warbandBankFrame = CreateFrame("Frame")
-warbandBankFrame:RegisterEvent("BANKFRAME_OPENED")
-warbandBankFrame:SetScript("OnEvent", function()
-  C_Timer.After(0, function()
-    BankFrameTab3:Click()
-  end)
-end)
