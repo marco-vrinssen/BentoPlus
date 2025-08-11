c81e318b7919d01279c6b1dfe40c5f583a93e71b
Marco Vrinssen <118135838+marco-vrinssen@users.noreply.github.com>
2025-08-11 17:13:30 +0200

    feat: Implement right-click reload functionality with tooltip for main menu button

---
+++ Modules/ReloadClick.lua
@@ -0,0 +1,24 @@
+-- Create tooltip frame for main menu button
+
+local menuTooltip = CreateFrame("GameTooltip", "BentoPlusMenuTooltip", UIParent, "GameTooltipTemplate")
+
+-- Add right-click reload and tooltip for main menu button
+
+MainMenuMicroButton:HookScript("OnClick", function(self, mouseButton)
+  if mouseButton == "RightButton" then
+    ReloadUI()
+  end
+end)
+
+MainMenuMicroButton:HookScript("OnEnter", function()
+  menuTooltip:SetOwner(GameTooltip, "ANCHOR_NONE")
+  menuTooltip:SetPoint("TOPLEFT", GameTooltip, "BOTTOMLEFT", 0, -2)
+  menuTooltip:SetPoint("TOPRIGHT", GameTooltip, "BOTTOMRIGHT", 0, -2)
+  menuTooltip:ClearLines()
+  menuTooltip:AddLine("Right-Click: Reload UI", 1, 1, 1)
+  menuTooltip:Show()
+end)
+
+MainMenuMicroButton:HookScript("OnLeave", function()
+  menuTooltip:Hide()
+end)
